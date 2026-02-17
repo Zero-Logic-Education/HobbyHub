dd71d196647c8fd6752140d79021bf9af0d4381e
SNOWNUMB <mamayev.stas@gmail.com>
2026-02-17 23:29:33 +0500

    feat: добавлен функционал обновления профиля родителя на экране согласия

--- app/lib/ui/onboarding/parental_consent_screen.dart
+++ app/lib/ui/onboarding/parental_consent_screen.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import '../../core/theme/app_colors.dart';
 import '../../core/theme/app_spacing.dart';
 import '../../core/theme/app_typography.dart';
 import '../../core/router/app_router.dart';
+import '../../providers/user_provider.dart';
 import '../shared/app_button.dart';
 import '../shared/app_text_field.dart';
 
-class ParentalConsentScreen extends StatefulWidget {
+class ParentalConsentScreen extends ConsumerStatefulWidget {
   const ParentalConsentScreen({super.key});
 
   @override
-  State<ParentalConsentScreen> createState() => _ParentalConsentScreenState();
+  ConsumerState<ParentalConsentScreen> createState() =>
+      _ParentalConsentScreenState();
 }
 
-class _ParentalConsentScreenState extends State<ParentalConsentScreen> {
+class _ParentalConsentScreenState extends ConsumerState<ParentalConsentScreen> {
   final _emailController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
 
-  void _submit() {
+  Future<void> _submit() async {
     if (!_formKey.currentState!.validate()) return;
-    // TODO: Отправить запрос родителю или сохранить email
-    context.push(AppRoutes.home);
+
+    final parentEmail = _emailController.text.trim();
+    final notifier = ref.read(currentUserProfileNotifierProvider.notifier);
+
+    await notifier.updateProfile(parentEmail: parentEmail);
+
+    if (mounted) {
+      context.push(AppRoutes.interestsSelection);
+    }
   }
 
   @override

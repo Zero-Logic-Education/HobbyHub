375af3495dfcda7510a0ef068620f3d1cabc59e4
SNOWNUMB <mamayev.stas@gmail.com>
2026-02-24 14:55:02 +0500

    refactor: Обновлены методы получения цвета с использованием `withValues` для улучшения читаемости и консистентности кода

--- lib/ui/welcome/welcome_screen.dart
+++ lib/ui/welcome/welcome_screen.dart
@@ -32,7 +32,7 @@ class WelcomeScreen extends StatelessWidget {
                           shape: BoxShape.circle,
                           boxShadow: [
                             BoxShadow(
-                              color: AppColors.primary.withOpacity(0.3),
+                              color: AppColors.primary.withValues(alpha: 0.3),
                               blurRadius: 15,
                               offset: const Offset(0, 5),
                             ),
